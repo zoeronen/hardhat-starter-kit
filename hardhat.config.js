bf64447e60d752d9e3e2b6dfda13b887467d6678
Zoe Ronen <zoeronen@email.com>
2022-10-15 18:08:50 -0400

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,14 +20,14 @@ const POLYGON_MAINNET_RPC_URL =
   process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const GOERLI_RPC_URL =
   process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "your-private-key"
 // optional
-const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+const MNEMONIC = process.env.MNEMONIC || "your-mnemonic"
 const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "your-api-key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "your-api-key"
 const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
